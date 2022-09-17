531907d36a7bfc62aeda23389a01e166d86eb2d7
Ekaterina Efremova <katerina.usatykh@gmail.com>
2022-09-17 14:38:42 +0300

    edit main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,29 @@
-fun main(args: Array<String>) {
-   
+package ru.netology
+
+data class Post(
+    val id: Int,
+    val ownerId: Int,
+    val fromId: Int,
+    val date: Int,
+    val text: String,
+    val friendsOnly: Boolean,
+    var likes: Likes,
+    val postType: String,
+    val isPinned: Boolean,
+    val markedAsAds: Boolean
+)
+
+object WallService {
+    private var posts = emptyArray<Post>()
+    }
+
+data class Likes(
+    val count: Int,
+    val userLikes: Boolean,
+    val canLike: Boolean,
+    val canPublish: Boolean
+)
+
+fun main() {
+
 }
